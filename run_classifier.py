cb3b18893c1b686ed4e1476dbedd5b7bf0b48a3b
typijo <t.yama.pijotts@gmail.com>
2019-02-15 16:37:40 +0700

    bug fixed

--- run_classifier.py
+++ run_classifier.py
@@ -392,6 +392,7 @@ class TltcProcessor(DataProcessor):
     
     if method_undersample != None:
       examples = self._undersample_examples(examples, method_undersample)
+    print(examples)
     
     return examples
 
