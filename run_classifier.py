5cd7f298f4da846870c13811a67bff2c0ceb20d9
yamakoshi <yamakoshi@kl.i.is.nagoya-u.ac.jp>
2019-05-20 13:58:01 +0900

    cid feature available (cid visible in example view)

--- run_classifier.py
+++ run_classifier.py
@@ -609,6 +609,9 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
   assert len(input_mask) == max_seq_length
   assert len(segment_ids) == max_seq_length
 
+  # *ADDED* cid feature
+  input_cid = example.cid
+
   label_id = label_map[example.label]
   if ex_index < 5:
     tf.logging.info("*** Example ***")
@@ -618,10 +621,8 @@ def convert_single_example(ex_index, example, label_list, max_seq_length,
     tf.logging.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
     tf.logging.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
     tf.logging.info("segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
+    tf.logging.info("cid: %s" % input_cid)
     tf.logging.info("label: %s (id = %d)" % (example.label, label_id))
-  
-  # *ADDED* cid feature
-  input_cid = example.cid
   
   feature = InputFeatures(
       input_ids=input_ids,
