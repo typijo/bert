27cfe7a63c7b13ab2e340f235b7d91006865a00f
yamakoshi <yamakoshi@kl.i.is.nagoya-u.ac.jp>
2019-07-02 14:55:57 +0900

    bug fixed on sqrt sampling

--- run_classifier.py
+++ run_classifier.py
@@ -557,7 +557,8 @@ class TltcProcessorWithGlobalTid(TltcProcessorWithCid):
       num_sample = min(len(examples), multiplier*math.sqrt(len(examples)))
       examples_selected = random.sample(examples, int(num_sample))
       examples_return += examples_selected
-      random.shuffle(examples_return)
+    
+    random.shuffle(examples_return)
     
     return examples_return
 
